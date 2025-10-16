40fba82a23c243ab04754cf03762586cdf9dbb18
naiaMonde <naia.mondeteguy@gmail.com>
2025-10-16 11:18:46 +0200

    page panier avec son lien depuis index

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+    session_start();
+
     //Connection à la base
     $pdo = new PDO('mysql:host=lakartxela.iutbayonne.univ-pau.fr;dbname=nmondeteguy_pro', 'nmondeteguy_pro', 'nmondeteguy_pro');
 
@@ -7,7 +10,9 @@
     $stmt->execute();
 
     $figurines = $stmt->fetchAll(PDO::FETCH_ASSOC); // tableau de lignes
+
 ?>
+
 <!doctype html>
 <html lang="fr">
     <head>
@@ -17,13 +22,13 @@
 <body>
 <header> 
     <!-- Logo --> 
-    <a href="page_accueil.html"> 
+    <a href="index.php"> 
     <img src="" alt="Logo" > <!-- ICI --> 
     </a> 
     <!-- Panier --> 
     <a href="panier.php" title="Lien vers le panier"> Panier </a> 
     <!-- Back Office --> 
-    <a href="backOffice.php" title="Lien vers le back office"> S'identifier </a> 
+    <a href="connexion.php" title="Lien vers le back office"> S'identifier </a> 
 </header>
     <main>
         <section>
@@ -31,18 +36,43 @@
             <?php foreach ($figurines as $figurine): 
                 $idFig = htmlspecialchars($figurine["id"]); 
                 ?>
-                <li>
-                    <a href="article.php?id=<?= $idFig ?>" >
-                        <img src="images/<?= htmlspecialchars($figurine['image'] ?? '')?> ">
-                        <h3><?= htmlspecialchars($figurine['nom'] ?? '') ?></h3>
-                        <h4><?= htmlspecialchars($figurine['prix'] ?? '') ?> €</h4>
-                        <p>Quantité : <?= (int)($figurine['quantite'] ?? 0) ?></p>
-                        <button type="button">Ajouter au panier</button>
-                    </a>
-                </li>
-            <?php endforeach;?>
+                    <li>
+                        <a href="article.php?id=<?= $idFig ?>" >
+                            <img src="images/<?= htmlspecialchars($figurine['image'] ?? '')?> ">
+                            <h3><?= htmlspecialchars($figurine['nom'] ?? '') ?></h3>
+                            <h4><?= htmlspecialchars($figurine['prix'] ?? '') ?> €</h4>
+                            <p>Quantité : <?= (int)($figurine['quantite'] ?? 0) ?></p>
+                        </a>
+                        
+                        <form method="POST">
+                            <input type="hidden" name="idFig" value="<?= $idFig ?>">
+                            <button type="submit">Ajouter au panier</button>
+                        </form>
+                    </li>
+            <?php 
+                endforeach;
+                // Si le bouton a été cliqué
+                if (isset($_POST['idFig'])) {
+                    $idFig = (int)$_POST['idFig'];
+                // On ajoute l'id dans la session (panier)
+                if (!isset($_SESSION['panier'])) {
+                        $_SESSION['panier'] = [];
+                    }
+
+                // Si l’article n’existe pas encore, on l’ajoute
+                if (isset($_SESSION['panier'])) {
+                        $_SESSION['panier'][] = $idFig;
+                    }
+                }
+                var_dump($_SESSION);
+            ?>
             </ul>
         </section>
     </main>
+    <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
 </body>
 </html>
